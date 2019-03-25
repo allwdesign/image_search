089827f647db2ec6b4ff3172f33c261311053261
Natalia Kuzmenko <allwdesign@gmail.com>
2019-03-25 18:28:35 +0300

    Add some styles for templates

--- app/views.py
+++ app/views.py
@@ -7,11 +7,10 @@ class PhotoUploadView(CreateView):
     model = Photo
     fields = ['image']
     template_name = 'app/base.html'  # <app>/<model>_<viewtype>.html
-    success_url = '/results/'  # /result/ if in url path('results/',...name='something')
+    success_url = '/results/'  # /results/ if in url path('results/',...name='something')
 
 
 class SearchResultsView(ListView):
     model = Photo
     template_name = 'app/results.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'results'
-
